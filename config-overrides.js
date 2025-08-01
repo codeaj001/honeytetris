145e7b6f6200f6854a9ac4c83e54f84d9123b85f
codeaj001 <codeaj001@gmail.com>
2025-08-01 22:12:14 +0100

    Tetris working with ES error

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify")
+  };
+  return config;
+};
